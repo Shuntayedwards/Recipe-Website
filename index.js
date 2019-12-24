35c567a778befff9bb23b1fa562a05a0518ca364
Shuntay Edwards <shuntayedwards@aol.com>
2019-12-23 23:59:34 -0800

    Recipe website in process

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/')));
+
+const PORT = process.env.PORT || 8888;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
